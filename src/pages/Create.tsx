51f28c54be9473d3d5cbec97707582c190bc491b
Tuvi9 <kaspupavel@gmail.com>
2025-02-03 09:36:28 +0200

    Implemented Multer in the front-end of account creation.

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -9,26 +9,29 @@ function Create () {
                 <div className='pb-4'>
                     <h1 className='text-2xl text-center text-white font-extrabold text-stroke'>Profile picture</h1>
                 </div>
-                <div className='flex justify-center pb-4'>
-                    <button className='w-24 h-24 rounded-full bg-gray border-3 flex items-center justify-center'>
-                        <div className='text-white text-2xl font-extrabold text-stroke'>PNG</div>
-                    </button>
-                </div>
+                <form action='/profile' method='post' encType='multipart/form-data'>
+                    <div className='flex justify-center pb-4'>
+                        <label className='w-24 h-24 rounded-full bg-gray border-3 border-black flex items-center justify-center text-2xl text-white font-extrabold text-stroke'>
+                            PNG
+                            <input className='hidden' type='file' name='avatar'/>
+                        </label>
+                    </div>
+                </form>
                 <form>
                     <div className='flex flex-col gap-4 items-center'>
                         <div className='flex justify-center'>
                             <label htmlFor='username'></label>
-                            <input className='border-3 border-black rounded-full p-2 text-2xl font-extrabold bg-gray placeholder-white placeholder-stroke w-[70%] text-white' type='text' id='username' placeholder='Username'></input>
+                            <input className='border-3 border-black rounded-full p-2 text-2xl placeholder:font-extrabold bg-gray placeholder-white placeholder-stroke w-[70%] text-white focus:outline-none' type='text' id='username' placeholder='Username'></input>
                             <br/>
                         </div>
                         <div className='flex justify-center'>
                             <label htmlFor='email'></label>
-                            <input className='border-3 border-black rounded-full p-2 text-2xl font-extrabold bg-gray placeholder-white placeholder-stroke w-[70%] text-white' type='text' id='email' placeholder='Email'></input>
+                            <input className='border-3 border-black rounded-full p-2 text-2xl placeholder:font-extrabold bg-gray placeholder-white placeholder-stroke w-[70%] text-white focus:outline-none' type='text' id='email' placeholder='Email'></input>
                             <br/>
                         </div>
                         <div className='flex justify-center'>
                             <label htmlFor='password'></label>
-                            <input className='border-3 border-black rounded-full p-2 text-2xl font-extrabold bg-gray placeholder-white placeholder-stroke w-[70%] text-white' type='text' id='password' placeholder='Password'></input>
+                            <input className='border-3 border-black rounded-full p-2 text-2xl placeholder:font-extrabold bg-gray placeholder-white placeholder-stroke w-[70%] text-white focus:outline-none' type='text' id='password' placeholder='Password'></input>
                         </div>
                         <div className='flex flex-row w-[70%] justify-center gap-x-4'>
                             <div className='border-3 rounded-full p-2 bg-gray font-extrabold text-stroke text-2xl text-center w-[100px]'>
